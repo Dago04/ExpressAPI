056c37febdde13af010f25fff6b4cec218ae0903
Dago Salas <DagoSalas1999@gmail.com>
2025-08-03 16:53:01 -0600

    Adding new blog schema

---
+++ models/Blog.js
@@ -0,0 +1,26 @@
+const moongose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+
+    title:{
+        type: String,
+        required: true,
+        maxlength: 100,
+        trim:true
+    },
+    summary:{
+        type: String,
+        required: true,
+        maxlength: 500,
+        trim:true
+    },
+    author:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+
+}, { timestamps: true })
+
+
+module.exports = mongoose.model('Blog', blogSchema);
