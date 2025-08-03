33f5cc11f6056b297deef4739c5eb68e944cd61b
Dago Salas <DagoSalas1999@gmail.com>
2025-08-03 16:44:22 -0600

    Adding new user schema

--- models/User.js
+++ models/User.js
@@ -1,16 +1,27 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        maxlength: 50
-    },
-    job: {
-        type: String,
-        required: true,
-        maxlength: 50
-    }
-}, { timestamps: true });
+const profileSchema = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    age: {type: Number, required: true, min: 0},
+    phoneNumber: {type: String, required: true, match: /^\d{10}$/},
+    userDescription: {type: String, required: true, maxlength: 500},
+    job: {type: String, required: true, maxlength: 100},
+}, {_id:false});
 
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profile: profileSchema
+}, {
+  timestamps: true
+});
 module.exports = mongoose.model('User', userSchema);
