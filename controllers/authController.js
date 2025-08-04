ee5d5cc3ceeb090d4e1859b90f48ecce10644d21
Dago Salas <DagoSalas1999@gmail.com>
2025-08-04 13:07:58 -0600

    Se realizan mejoras en el authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,30 @@
 const jwt  = require('jsonwebtoken');
 const User = require('../models/User');
+const asyncHandler = require('../utils/asyncHandler');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-exports.register = async (req,res,next) => {
+exports.register = asyncHandler(async (req, res, next) => {
+  try {
     const user = await User.create(req.body);
-    res.status(201).json({ id: user._id });
-};
-
-exports.login = async (req, res, next) => {
+    return res.status(201).json({ id: user._id });
+  } catch (err) {
+    if (err.code === 11000) {
+      // Clave duplicada en Mongo (email único)
+      return res.status(409).json({ message: 'Email ya registrado' });
+    }
+    throw err; // será capturado por asyncHandler
+  }
+});
+exports.login = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email }).select('+password');
+
+  // Validación de credenciales
   if (!user || !(await user.comparePassword(password))) {
     return res.status(401).json({ message: 'Credenciales inválidas' });
   }
   const token = signToken(user._id);
   res.json({ token });
-};
+});
