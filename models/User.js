fd3ee871cb9a30d27068b8b73ec407b356ef77a2
Dago Salas <DagoSalas1999@gmail.com>
2025-08-04 14:12:03 -0600

    Se actualiza el user model

--- models/User.js
+++ models/User.js
@@ -36,6 +36,21 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+userSchema.pre('findOneAndUpdate', async function (next) {
+  const update = this.getUpdate();
+
+  // Soporta variantes con y sin $set
+  const plainPassword = update.password || (update.$set && update.$set.password);
+  if (!plainPassword) return next();
+
+  const hashed = await bcrypt.hash(plainPassword, 12);
+
+  if (update.password)               update.password               = hashed;
+  if (update.$set && update.$set.password) update.$set.password = hashed;
+
+  next();
+});
+
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();          // evita re-hash innecesario
   this.password = await bcrypt.hash(this.password, 12);     // work-factor 12 ≈ producción
@@ -44,4 +59,6 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.comparePassword = function (candidate) {
   return bcrypt.compare(candidate, this.password);          // verificación constante-time
 };
+
+
 module.exports = mongoose.model("User", userSchema);
